9f569a40636e27521d4e6d86d4deae82b30ec269
Atul Raina <118076922+atul-raina23@users.noreply.github.com>
2025-01-08 23:03:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { cloudnairyconnect } = require("./config/cloudinary");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8000;
 database.connect();
 
 app.use(express.json());
